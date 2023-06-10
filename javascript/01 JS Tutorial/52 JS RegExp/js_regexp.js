72e83522f0424c2070e8b8ff9e41df730ba30f3b
Thanh Pham <thanhpham091790@gmail.com>
2023-06-10 15:09:57 -0600

    Building a regular expression from dynamic inputs.

--- javascript/01 JS Tutorial/52 JS RegExp/js_regexp.js
+++ javascript/01 JS Tutorial/52 JS RegExp/js_regexp.js
@@ -34,3 +34,27 @@ regex.lastIndex = 5;
 console.log(regex.test(str)); // false (lastIndex is taken into account with sticky flag)
 
 regex.lastIndex; // 0 (reset after match failure)
+
+/* The difference between the sticky flag and the global flag */
+{
+    const re = /\d/y;
+    let r;
+    while ((r = re.exec("123 456"))) {
+        console.log(r, "AND re.lastIndex", re.lastIndex);
+    }
+}
+
+/* Extracting subdomain name from URL */
+{
+    const url = "https://www.thanhhpham.com";
+    console.log(/^https?:\/\/(.+?)\./.exec(url)[1]);
+}
+
+/* Building a regular expression from dynamic inputs */
+{
+    const breakfasts = ["bacon", "eggs", "oatmeal", "toast", "cereal"];
+    const order = "Let me get some bacon and eggs, please";
+
+    console.log(order.match(new RegExp(`\\b(${breakfasts.join("|")})\\b`, "g")));
+}
+
