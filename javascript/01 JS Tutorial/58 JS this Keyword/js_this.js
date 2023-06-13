4b571b127a6ab80fc662e4463b27a02fd4e68f80
Thanh Pham <thanhpham091790@gmail.com>
2023-06-13 07:15:00 -0600

    Variable declared with var become properties of the global object.

---
+++ javascript/01 JS Tutorial/58 JS this Keyword/js_this.js
@@ -0,0 +1,15 @@
+
+/* This in function context */
+
+// An object can be passed as the first argument to call
+// or apply and this will be bound to it.
+const obj = { a: "Custom" };
+
+// Variable declared with var become properties of the global object.
+var a = "Global";
+
+function whatsThis() {
+    return this.a; // The value of this is dependent on how the function is called.
+}
+
+console.log(whatsThis()); // 'Global'; this in function isn't set, so it defaults to the global/window objecdt in non-strict mode.
