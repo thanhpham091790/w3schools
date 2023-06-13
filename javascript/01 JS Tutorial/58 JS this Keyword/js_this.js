289ccd4414b4710619f039a258259214bbda1c8d
Thanh Pham <thanhpham091790@gmail.com>
2023-06-13 07:25:38 -0600

    The first parameter is the object to use as 'this'

--- javascript/01 JS Tutorial/58 JS this Keyword/js_this.js
+++ javascript/01 JS Tutorial/58 JS this Keyword/js_this.js
@@ -15,3 +15,14 @@ function whatsThis() {
 console.log(whatsThis()); // 'Global'; this in function isn't set, so it defaults to the global/window objecdt in non-strict mode.
 obj.whatsThis = whatsThis;
 console.log(obj.whatsThis()); // 'Custom'; this in the function is set to obj
+
+function add(c, d) {
+    return this.a + this.b + c + d;
+}
+
+const o = { a: 1, b: 3 };
+
+// The first parameter is the object to use as 'this'; subsequent
+// parameters are used as arguments in the function call
+console.log(add.call(o, 5, 7));
+
