5a31382493eb4bb386a6df8ab93942a71789a3c8
Thanh Pham <thanhpham091790@gmail.com>
2023-06-13 07:49:59 -0600

    Calling f.bind(someObject) creates a new function with the same body and scope as f, but the value of this is permanently bound to the first argument of bind, regardless of how the function is being called.

--- javascript/01 JS Tutorial/58 JS this Keyword/js_this.js
+++ javascript/01 JS Tutorial/58 JS this Keyword/js_this.js
@@ -1,31 +1,58 @@
 
 /* This in function context */
+{
 
-// An object can be passed as the first argument to call
-// or apply and this will be bound to it.
-const obj = { a: "Custom" };
+    // An object can be passed as the first argument to call
+    // or apply and this will be bound to it.
+    const obj = { a: "Custom" };
 
-// Variable declared with var become properties of the global object.
-var a = "Global";
+    // Variable declared with var become properties of the global object.
+    var a = "Global";
 
-function whatsThis() {
-    return this.a; // The value of this is dependent on how the function is called.
+    function whatsThis() {
+        return this.a; // The value of this is dependent on how the function is called.
+    }
+
+    console.log(whatsThis()); // 'Global'; this in function isn't set, so it defaults to the global/window objecdt in non-strict mode.
+    obj.whatsThis = whatsThis;
+    console.log(obj.whatsThis()); // 'Custom'; this in the function is set to obj
+
+    function add(c, d) {
+        return this.a + this.b + c + d;
+    }
+
+    const o = { a: 1, b: 3 };
+
+    // The first parameter is the object to use as 'this'; subsequent
+    // parameters are used as arguments in the function call
+    console.log(add.call(o, 5, 7));
+
+    // The first parameter is the object to use as 'this', the second is an 
+    // array whose members are used as arguments in the function call
+    console.log(add.apply(o, [10, 20]));
 }
 
-console.log(whatsThis()); // 'Global'; this in function isn't set, so it defaults to the global/window objecdt in non-strict mode.
-obj.whatsThis = whatsThis;
-console.log(obj.whatsThis()); // 'Custom'; this in the function is set to obj
-
-function add(c, d) {
-    return this.a + this.b + c + d;
+/* This and object conversion */
+function bar() {
+    console.log(Object.prototype.toString.call(this));
 }
 
-const o = { a: 1, b: 3 };
+bar.call(7);
+bar.call("foo");
+bar.call(undefined);
 
-// The first parameter is the object to use as 'this'; subsequent
-// parameters are used as arguments in the function call
-console.log(add.call(o, 5, 7));
+/* The bind() method */
+{
+    function f() {
+        return this.a;
+    }
 
-// The first parameter is the object to use as 'this', the second is an 
-// array whose members are used as arguments in the function call
-console.log(add.apply(o, [10, 20]));
+    const g = f.bind({ a: "azerty" });
+    console.log(g()); // azerty
+
+    const h = g.bind({ a: "yoo" }); // bind only works once!
+    console.log(h()); // azerty
+
+    const o = { a: 37, f, g, h };
+    console.log(o.a, o.f(), o.g(), o.h());
+}
