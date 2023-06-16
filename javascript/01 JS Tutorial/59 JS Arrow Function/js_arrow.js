3119a9c05f7a3fad5c84ed23fe3d2fb4ea38d950
Thanh Pham <thanhpham091790@gmail.com>
2023-06-16 06:33:30 -0600

    Using arrow functions

--- javascript/01 JS Tutorial/59 JS Arrow Function/js_arrow.js
+++ javascript/01 JS Tutorial/59 JS Arrow Function/js_arrow.js
@@ -18,3 +18,26 @@ const arr = [5, 6, 13, 0, 1, 18, 23];
 
 const sum = arr.reduce((a, b) => a + b);
 console.log(sum);
+
+const even = arr.filter((a) => a % 2 === 0);
+console.log(even);
+
+const double = arr.map((a) => a * 2);
+console.log(double);
+
+// More concise promise chains
+// promise
+//     .then((a) => {
+//         // ...
+//     })
+//     .then((a) => {
+//         // ...
+//     });
+
+// Parameterless arrow functions that are visually easier to parse
+setTimeout(() => {
+    console.log("I happen sooner");
+    setTimeout(() => {
+        console.log("I happen later");
+    }, 1);
+}, 1);
