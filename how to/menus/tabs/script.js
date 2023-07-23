7dac87ba71412cbb16bd85f8a8f92d83f297bd4c
Thanh Pham <thanhpham091790@gmail.com>
2023-07-22 22:38:15 -0600

    How to create a tab with css and javascript.

---
+++ how to/menus/tabs/script.js
@@ -0,0 +1,20 @@
+function openTab(event, tabId) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabId).style.display = "block";
+    event.currentTarget.classList.add("active");
+}
