884179d312e1ad44c1f7c9f1d475fcf68f2f5fa1
Thanh Pham <thanhpham091790@gmail.com>
2023-07-25 08:04:22 -0600

    How to create a responsive top navigation bar

---
+++ how to/menus/responsive-topnav/script.js
@@ -0,0 +1,9 @@
+// Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.classList.contains("responsive")) {
+        x.classList.remove("responsive");
+    } else {
+        x.classList.add("responsive");
+    }
+}
