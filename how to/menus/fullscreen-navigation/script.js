d724be8bd1a139c6e8a2ab8a690a54dbd6669c70
Thanh Pham <thanhpham091790@gmail.com>
2023-07-28 08:28:23 -0600

    Fullscreen navigation without animation.

---
+++ how to/menus/fullscreen-navigation/script.js
@@ -0,0 +1,11 @@
+// Open when someone clicks on the span
+function openNav() {
+    // document.getElementById("myNav").style.width = "100%";
+    document.getElementById("myNav").style.display = "block";
+}
+
+// Close when someone clicks on the "x" symbol inside the overlay
+function closeNav() {
+    // document.getElementById("myNav").style.width = "0%";
+    document.getElementById("myNav").style.display = "none";
+}
