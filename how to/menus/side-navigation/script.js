600d3b843d1446090718e770ac1448a5d3a9adb3
Thanh Pham <thanhpham091790@gmail.com>
2023-07-28 07:07:51 -0600

    Side navigation without animation.

--- how to/menus/side-navigation/script.js
+++ how to/menus/side-navigation/script.js
@@ -3,7 +3,8 @@ function openNav() {
     // document.getElementById("mySidenav").style.width = "250px";
     // document.getElementById("main").style.marginLeft = "250px";
     // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    document.getElementById("mySidenav").style.width = "100%";
+    // document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.display = "block";
 }
 
 // Set the width of the side navigation to 0px
@@ -12,5 +13,6 @@ function closeNav() {
     // document.getElementById("mySidenav").style.width = "0";
     // document.getElementById("main").style.marginLeft = "0";
     // document.body.style.backgroundColor = "white";
-    document.getElementById("mySidenav").style.width = "0";
+    // document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.display = "none";
 }
