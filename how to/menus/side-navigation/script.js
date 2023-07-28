f657c5ba206c3bb71d782edf31459e847bbb5a29
Thanh Pham <thanhpham091790@gmail.com>
2023-07-28 07:02:16 -0600

    Side navigation push content with opacity.

--- how to/menus/side-navigation/script.js
+++ how to/menus/side-navigation/script.js
@@ -1,10 +1,14 @@
 // Set the width of the side navigation to 250px
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 // Set the width of the side navigation to 0px
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
 }
