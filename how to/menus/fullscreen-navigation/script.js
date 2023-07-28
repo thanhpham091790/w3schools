0dfa4038904175f49b2c97898988d85da16ebe11
Thanh Pham <thanhpham091790@gmail.com>
2023-07-28 08:30:11 -0600

    Slide down the menu from the top.

--- how to/menus/fullscreen-navigation/script.js
+++ how to/menus/fullscreen-navigation/script.js
@@ -1,11 +1,13 @@
 // Open when someone clicks on the span
 function openNav() {
     // document.getElementById("myNav").style.width = "100%";
-    document.getElementById("myNav").style.display = "block";
+    // document.getElementById("myNav").style.display = "block";
+    document.getElementById("myNav").style.height = "100%";
 }
 
 // Close when someone clicks on the "x" symbol inside the overlay
 function closeNav() {
     // document.getElementById("myNav").style.width = "0%";
-    document.getElementById("myNav").style.display = "none";
+    // document.getElementById("myNav").style.display = "none";
+    document.getElementById("myNav").style.height = "0%";
 }
